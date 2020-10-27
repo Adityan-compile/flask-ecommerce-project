12ed9b686ad4f96efaf93849d4155ae1d41d0127
Adityan <>
2020-10-27 10:20:22 +0530

    .

--- app.py
+++ app.py
@@ -3,13 +3,17 @@ from datetime import timedelta
 import sqlalchemy
 
 app = Flask(__name__)
+app.secret_key = "awwep932089rjdnc+_{EFWVWDVDASC+};:}WE{V}]D_WS_O!_+0id-0wkWS+"
 
 
 @app.route("/home")
 def home():
-    return render_template("index.html.jinja", title="home", content="Hello"), 200
-    
+    return render_template("index.html.jinja", title="Home", content="Home"), 200
 
+@app.route("/redirect")
+def redirection():
+    print("Redirected successfully")
+    return redirect(url_for("home")), 307
 
 if "__name__" == "__main__":
     app.run(debug=True)
