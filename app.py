a9d99cb5f3e65d8be77220c469d3be315216feb1
Adityan <>
2020-10-27 09:15:39 +0530

    .

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/home")
 def home():
-    return render_template("index.html", title="home", content="Hello")
+    return render_template("index.html.jinja", title="home", content="Hello")
 
 
 if "__name__" == "__main__":
