c40ddaf1c448588c8f6d067ef2cc07553b5c227e
Adityan <>
2020-10-29 12:51:47 +0530

    .

--- app.py
+++ app.py
@@ -1,39 +1,39 @@
-
-from flask import Flask, render_template 
-from flask import redirect, url_for
-from flask import request, session
-from flask import Blueprint, flash
-from datetime import timedelta
+import os
+from flask import Flask,render_template,url_for,request,session,flash,redirect	
 from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+#from werkzeug import secure_filename
+#from flask.ext.uploads import UploadSet,configure_uploads,IMAGES
+#from werkzeug.security import generate_password_hash, \
+#     check_password_hash
+#from flask_admin import Admin 
+#from flask_admin import Admin, BaseView, expose
+#from flask_admin.contrib.sqla import ModelView
+#import re
+#from models import *
+
+
+#app.config['UPLOAD_FOLDER']=folder
+#app.config['UPLOADED_PHOTOS_DEST']='static/images'
+
+#configure_uploads(app,photos)
 
 app = Flask(__name__)
-app.secret_key = "fc1dj4ad6s47dep932089rjdnc+_{EF37W998VW798DeqVDrewASewC+};:}WE{V}]D_WS_O!_+"
-app.permanent_session_lifetime = timedelta(days=30)
 
-#app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///users.sqlite3'
-#app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-#db = SQLAlchemy(app)
-
-# class users(db.Model):
-#    _id = db.coloumn("id", db.integer, primary_key = True)
-#    name = db.coloumn("name", db.String(100))
-#    email = db.coloumn("email", db.String(320))
-
-#    def __init__(self,):
-#        self.name = name
-#        self.email = email
-#        self.id = id
-
-@app.route("/home")
+@app.route('/home')
+@app.route('/')
 def home():
-    return render_template("index.html.jinja", title = "Home", content = "Home"), 200
+   return render_template("index.html.jinja")
 
-@app.route("/redirect")
-def redirection():
-    print("Redirected successfully")
-    return redirect(url_for("home")), 307
+@app.route('/admin')
+@app.route('/admin/home')
+def admin():
+   return render_template("admin.html.jinja")   
+
+@app.errorhandler(404)
+def error(e):
+  return render_template("404.html.jinja"), 200
 
 if "__name__" == "__main__":
-  # db.create_all()
+    #db.create_all()
     app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template
+from flask import redirect, url_for
+from flask import request, session
+from flask import Blueprint, flash
+from datetime import timedelta
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+
+app.secret_key = "fc1dj4ad6s47dep932089rjdnc+_{EF37W998VW798DeqVDrewASewC+};:}WE{V}]D_WS_O!_+"
+app.permanent_session_lifetime = timedelta(days=30)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'products.sqlite')
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+
+db = SQLAlchemy(app)
+
+class products(db.Model):
+    _id = db.coloumn(db.integer, primary_key=True)
+    _product_name = db.coloumn( db.String(100))
+    _product_price = db.coloumn(db.String(8))
+    _product_brand = db.coloumn(db.String(100))
+    _product_img = db.coloumn(db.String(100))
+    _product_description = db.coloumn(db.String(2000))
+    def __init__(self):
+        self._id = _id
+        self._product_name = _product_name
+        self._product_price = _product_price
+        self._product_brand = _product_brand
+        self._product_img = _product_img
+        self._product_description = _product_description
+
+
+
+class cart(db.Model):
+	serial_no=db.Column(db.Integer,primary_key=True)
+	product_id=db.Column(db.Integer)
+	customer_id=db.Column(db.Integer)
+	product_name=db.Column(db.String(100))
+	product_image=db.Column(db.String(50))
+	def __init__(self,product_id,customer_id,product_name,product_image):
+		self.product_id=product_id
+		self.customer_id=customer_id
+		self.product_name=product_name
+		self.product_image=product_image
