ae8801abea53f845f367fd2058364ef8f7a74952
Adityan <>
2020-10-30 19:06:34 +0530

    .

--- app.py
+++ app.py
@@ -1,28 +1,10 @@
-import os
+
 from flask import Flask,render_template,url_for,request,session,flash,redirect	
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
-from datetime import timedelta
-#from werkzeug import secure_filename
-#from flask.ext.uploads import UploadSet,configure_uploads,IMAGES
-#from werkzeug.security import generate_password_hash, \
-#     check_password_hash
-#from flask_admin import Admin 
-#from flask_admin import Admin, BaseView, expose
-#from flask_admin.contrib.sqla import ModelView
-#import re
+
 #from models import *
 
-
-#app.config['UPLOAD_FOLDER']=folder
-#app.config['UPLOADED_PHOTOS_DEST']='static/images'
-
-#configure_uploads(app,photos)
-
-app = Flask(__name__)
-
-app.secret_key = "fc1dj4ad6s47dep932089rjdnc+_{EF37W998VW798DeqVDrewASewC+};:}WE{V}]D_WS_O!_+"
-app.permanent_session_lifetime = timedelta(days=30)
 
 @app.route('/home')
 @app.route('/')
@@ -62,5 +44,5 @@ def error(e):
    return render_template("404.html.jinja")
 
 if '__name__' == '__main__':
-   #db.create_all()
+   db.create_all()
    app.run(debug=True)
--- models.py
+++ models.py
@@ -8,22 +8,32 @@ import os
 
 app = Flask(__name__)
 
+# configuring file uploads
+UPLOAD_FOLDER = '/path/to/the/uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+#configuring session storage
+app.secret_key = "fc1dj4ad6s47dep932089rjdnc+_{EF37W998VW798DeqVDrewASewC+};:}WE{V}]D_WS_O!_+"
+app.permanent_session_lifetime = timedelta(days=30)
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+#configuring the database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
 
 db = SQLAlchemy(app)
 
 class products(db.Model):
-    _id = db.coloumn(db.integer, primary_key=True)
-    _product_name = db.coloumn( db.String(100))
-    _product_price = db.coloumn(db.String(8))
-    _product_brand = db.coloumn(db.String(100))
-    _product_img = db.coloumn(db.String(100))
-    _product_description = db.coloumn(db.String(2000))
-    _product_stock = db.coloumn(db.String(10))
-    def __init__(self):
+    _id = db.column(db.integer, primary_key=True)
+    _product_name = db.column( db.String(100))
+    _product_price = db.column(db.String(8))
+    _product_brand = db.column(db.String(100))
+    _product_img = db.column(db.String(100))
+    _product_description = db.column(db.String(2000))
+    _product_stock = db.column(db.String(10))
+    def __init__(self,_id, _product_name,  _product_price,  _product_brand, _product_img, _product_description, _product_stock):
         self._id = _id
         self._product_name = _product_name
         self._product_price = _product_price
@@ -40,7 +50,7 @@ class cart(db.Model):
 	customer_id=db.Column(db.Integer)
 	product_name=db.Column(db.String(100))
 	product_image=db.Column(db.String(50))
-	def __init__(self,product_id,customer_id,product_name,product_image):
+	def __init__(self, product_id, customer_id, product_name, product_image):
 		self.product_id=product_id
 		self.customer_id=customer_id
 		self.product_name=product_name
@@ -48,14 +58,14 @@ class cart(db.Model):
 
 
 class users(db.Model):
-    user_id=db.coloumn(db.integer,primary_key=True)
-    user_name=db.coloumn(db.String(500))
-    user_email=db.coloumn(db.string(500))
-    user_password=db.coloumn(db.String(500))
-    user_address=db.coloumn(db.String(1000))
-    user_city=db.coloumn(db.String(500))
-    user_state=db.coloumn(db.String(500))
-    user_zip=db.coloumn(db.String(50))
+    user_id=db.column(db.integer,primary_key=True)
+    user_name=db.column(db.String(500))
+    user_email=db.column(db.string(500))
+    user_password=db.column(db.String(500))
+    user_address=db.column(db.String(1000))
+    user_city=db.column(db.String(500))
+    user_state=db.column(db.String(500))
+    user_zip=db.column(db.String(50))
     def __init__(self, user_id, user_name, user_email, user_password, user_address,  user_city, user_state, user_zip):
         self.user_id=user_id
         self.user_name=user_name
