cb3e16a8d260c2a3f032133f7e6e065615d0a711
Adityan <>
2020-11-04 12:05:25 +0530

    added admin auth

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, render_template, url_for, request, session, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
@@ -19,7 +20,10 @@ def home():
 @app.route('/admin/products')
 @app.route('/admin/')
 def admin():
-    return render_template("admin.html.jinja")
+    if "admin" in session:
+        return render_template("admin.html.jinja")
+    else:
+        return redirect('login')
 
 
 @app.route('/admin/delete')
@@ -35,18 +39,29 @@ def login():
         username = request.form['Name']
         password = request.form['Password']
         found_user = Users.query.filter_by(name=username).first()
+        found_admin = Admins.query.filter_by(name=username).first()
         if found_user.user_name == username:
-           if bcrypt.check_password_hash(found_user.password, password):
-               db.session.commit()
-               session['user'] = found_user.user_name
-               flash('Login Successful')
-               return redirect('home')
-           else:
-               flash('Incorrect username or password')
-               return redirect('login')
+            if bcrypt.check_password_hash(found_user.password, password):
+                      db.session.commit()
+                      session['user'] = found_user.user_name
+                      flash('Login Successful')
+                      return redirect('home')
+            else:
+                flash('Incorrect username or password')
+                return redirect('login')
+        elif found_admin.admin_name == username:
+            if found_admin.admin_password == password:
+                session['admin'] = username
+                return redirect('admin')
+            else:
+                flash('Incorrect username or password')
+                return redirect('login')
+       elif "user" in session:
+                flash('Already Logged in')
+                return redirect('home')
         else:
-            if "user" in session:
-                flash('Already Logged in') 
+            flash('Account not found')
+            return redirect('signup') 
     else:
         return render_template("login.html.jinja")
 
