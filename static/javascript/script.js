b060bc447aa22381e1e6ce443537ef43ebd95b01
Adityan <>
2020-12-09 13:24:00 +0530

    Added service worker for PWA

---
+++ static/javascript/script.js
@@ -0,0 +1,14 @@
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("serviceWorker.js")
+    .then((registration) => {
+      console.log("Service worker registered");
+      console.log(registration);
+    })
+    .catch((error) => {
+      console.log("Service worker error");
+      console.log(error);
+    });
+} else {
+  console.log("Service worker is not working");
+}
